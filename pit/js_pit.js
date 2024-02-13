0cf587e6831831df3c729486581657d231086a74
binivazqua <binizavazquez@gmail.com>
2024-02-13 08:35:13 -0600

    websocket shit

--- pit/js_pit.js
+++ pit/js_pit.js
@@ -1,3 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Your JavaScript code here
+    const form = document.querySelector("form"),
+        nextBtn = document.querySelector(".nextBtn"),
+        backBtn = document.querySelector(".backBtn"),
+        allInput = document.querySelectorAll(".first input");
+
+    nextBtn.addEventListener("click", () => {
+        const anyInputNotEmpty = Array.from(allInput).some(input => input.value.trim() !== "");
+
+        if (anyInputNotEmpty) {
+            form.classList.add('secActive');
+        } else {
+            console.log("Please fill in at least one input.");
+        }
+    });
+
+    backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+});
+
+/*
 const form = document.querySelector("form"),
     nextBtn = document.querySelector(".nextBtn"),
     backBtn = document.querySelector(".backBtn"),
@@ -15,3 +36,4 @@ nextBtn.addEventListener("click", ()=> {
 })
 
 backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+*/
